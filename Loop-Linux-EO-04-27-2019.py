bcb8618c3b721c0e207605d25158ffa71eebaeb4
Eric Oulster <40222141+ericoulster@users.noreply.github.com>
2019-05-10 00:37:41 -0400

    Delete Loop-Linux-EO-04-27-2019.py

--- Loop-Linux-EO-04-27-2019.py
+++
@@ -1,9 +0,0 @@
-from time import sleep
-import subprocess
-x = 1
-y = 1
-while x == 1:
-    print("iteration number: " + str(y))
-    subprocess.run(['python Pipeline-Deploy-EO-4-24-2019.py'], shell=True)
-    sleep(20)
-    y += 1
